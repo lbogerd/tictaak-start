8c33d74d183ea62bc98eda5b0df478b9787be890
lbogerd <l.bogerd@protonmail.com>
2025-07-03 16:55:56 +0200

    init

---
+++ src/router.tsx
@@ -0,0 +1,40 @@
+import { createRouter as createTanstackRouter } from "@tanstack/react-router";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+import "./styles.css";
+
+// Create a new router instance
+export const createRouter = () => {
+	const router = createTanstackRouter({
+		routeTree,
+		scrollRestoration: true,
+		defaultPreloadStaleTime: 0,
+		// Prevent the dev mode from complaining
+		defaultNotFoundComponent: ({ data }) => (
+			<div className="mt-10 text-center">
+				<h1 className="text-2xl font-bold">404</h1>
+				<p>Page not found</p>
+
+				{!!data && (
+					<>
+						<h2 className="text-xl font-bold">Data</h2>
+						<code className="text-sm font-mono">
+							{JSON.stringify(data, null, 2)}
+						</code>
+					</>
+				)}
+			</div>
+		),
+	});
+
+	return router;
+};
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: ReturnType<typeof createRouter>;
+	}
+}
