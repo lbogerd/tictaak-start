f32d30c58016598e75aafe4b4c7360fa65128242
lbogerd <l.bogerd@protonmail.com>
2025-07-21 22:17:33 +0200

    fix: update schema to reflect actual database

--- prisma/seed-data.ts
+++ prisma/seed-data.ts
@@ -3,10 +3,9 @@ import { PrismaClient } from "./client/client.ts"
 const db = new PrismaClient()
 
 for (const category of ["Category 1", "Category 2", "Category 3"]) {
-	await db.category.upsert({
-		where: { name: category },
-		update: {},
-		create: {
+	await db.category.create({
+		data: {
+			id: `cat-${category.split(" ")[1].toLowerCase()}`,
 			name: category,
 		},
 	})
@@ -17,6 +16,7 @@ const categories = await db.category.findMany()
 for (const category of categories) {
 	await db.task.create({
 		data: {
+			id: `${category.id}-task-1`,
 			title: `Task for ${category.name}`,
 			categoryId: category.id,
 		},
